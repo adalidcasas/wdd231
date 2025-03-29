c8c1c5ee7b17959155457ffbc873ed28de9f814e
adalidcasas <cas19040@byui.edu>
2025-03-29 18:55:54 -0400

    Directory page

---
+++ chamber/scripts/script.js
@@ -0,0 +1,16 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+navigation.addEventListener('click', function (e) {
+    const clickedElement = e.target;
+    title.textContent = clickedElement.textContent;
+    menuOptions.forEach(element => {
+        element.classList.remove('active')
+    });
+    clickedElement.classList.toggle('active');
+})
