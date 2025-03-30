0cbceca126e58c1e6a3999595fb8f6024d892078
adalidcasas <cas19040@byui.edu>
2025-03-29 23:26:47 -0400

    Directory page updates

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -28,7 +28,7 @@ const cards = document.querySelector('#gallery-cards');
 const displayMembers = (members) => {
     members.forEach((member) => {
         let card = document.createElement('section');
-        let name = document.createElement('h2');
+        let name = document.createElement('h3');
         let address = document.createElement('p');
         let phone = document.createElement('p');
         let website = document.createElement('p');
@@ -37,6 +37,9 @@ const displayMembers = (members) => {
         let slogan = document.createElement('p');
 
         card.className = 'gallery-card';
+        slogan.className = 'gallery-grid-only';
+        logo.className = 'gallery-grid-only';
+
         name.textContent = `${member.name}`;
         address.textContent = `${member.address}`;
         phone.textContent = `${member.phone_number}`;
@@ -44,7 +47,7 @@ const displayMembers = (members) => {
         mlevel.textContent = `${member.membership_level}`;
         slogan.textContent = `${member.slogan}`;
 
-        logo.setAttribute('src', member.image_file);
+        logo.setAttribute('src', `images/${member.image_file}`);
         logo.setAttribute('alt', `logo of ${member.name}`);
         logo.setAttribute('loading', 'lazy');
         logo.setAttribute('width', '340');
@@ -68,3 +71,19 @@ async function getMembersData() {
     displayMembers(data.members);
 }
 getMembersData()
+
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#gallery-cards");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("gallery-grid");
+    display.classList.remove("gallery-list");
+});
+
+listbutton.addEventListener("click", () => {
+    display.classList.remove("gallery-grid");
+    display.classList.add("gallery-list");
+});
+
