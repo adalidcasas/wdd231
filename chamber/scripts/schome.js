d2a3f4e8dd307a5e4160905183819008d9654120
adalidcasas <cas19040@byui.edu>
2025-04-05 19:29:39 -0400

    home page members

--- chamber/scripts/schome.js
+++ chamber/scripts/schome.js
@@ -30,10 +30,6 @@ let suns = document.createElement('p');
 
 function displayResults(data) {
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    const formater = new Intl.DateTimeFormat('en-US', {
-        hour: '2-digit',
-        minute: '2-digit'
-    });
 
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
@@ -60,12 +56,12 @@ function displayResults(data) {
     weatherDesc.appendChild(suns);
 }
 
-
 function toSentenceCase(str) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-const urlForecast = 'api.openweathermap.org/data/2.5/forecast?lat=-16.49&lon=-68.12&cnt=24&appid=2755dee7635a8a1a54c20016463edaf1';
+const weatherFore = document.querySelector('#wheater-forecast');
+const urlForecast = 'https://api.openweathermap.org/data/2.5/forecast?lat=-16.49&lon=-68.12&cnt=24&appid=2755dee7635a8a1a54c20016463edaf1';
 async function apiFetchForecast() {
     try {
         const response = await fetch(urlForecast);
@@ -80,8 +76,70 @@ async function apiFetchForecast() {
         console.log(error);
     }
 }
-apiFetch();
+apiFetchForecast();
+
+let weather_today = document.createElement('p');
+let weather_day2 = document.createElement('p');
+let weather_day3 = document.createElement('p');
 
 function displayResultsForecast(data) {
-    
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    weather_today.innerHTML = `Today: ${data.list[0].main.temp}&deg;F`;
+    weatherFore.appendChild(weather_today);
+
+    let unixTimestamp = data.list[8].dt;
+    let date = new Date(unixTimestamp * 1000);
+    let day = weekday[date.getDay()];
+
+    weather_day2.innerHTML = `${day}: ${data.list[8].main.temp}&deg;F`;
+    weatherFore.appendChild(weather_day2);
+
+    let unixTimestamp2 = data.list[16].dt;
+    let date2 = new Date(unixTimestamp2 * 1000);
+    let day2 = weekday[date2.getDay()];
+
+    weather_day3.innerHTML = `${day2}:${data.list[16].main.temp}&deg;F`;
+    weatherFore.appendChild(weather_day3);
 }
+
+const cards = document.querySelector('#section-members');
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h3');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('p');
+        let logo = document.createElement('img');
+        let mlevel = document.createElement('p');
+
+        name.textContent = `${member.name}`;
+        address.textContent = `${member.address}`;
+        phone.textContent = `${member.phone_number}`;
+        website.textContent = `${member.website_url}`;
+        mlevel.textContent = `${member.membership_level}`;
+        card.className = 'gallery-card';
+        logo.setAttribute('src', `images/${member.image_file}`);
+        logo.setAttribute('alt', `logo of ${member.name}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '340');
+        logo.setAttribute('height', '440');
+
+        card.appendChild(name);
+        card.appendChild(logo);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        cards.appendChild(card);
+    })
+}
+
+async function getMembersData() {
+    const response = await fetch('members.json');
+    const data = await response.json();
+    const datafiltered = data.members.filter(c => c.membership_level === 1 || c.membership_level === 2).slice(2);
+    displayMembers(datafiltered);
+}
+getMembersData()
