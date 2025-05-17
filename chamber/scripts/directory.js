c0a1465aff42f14b0c3d73439c765c6319582854
adalidcasas <cas19040@byui.edu>
2025-05-17 19:37:54 -0400

    corrections js

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -23,14 +23,14 @@ document.addEventListener("DOMContentLoaded", () => {
             const card = document.createElement('section');
             card.className = 'gallery-card';
             card.innerHTML = `
-            <h3>${name}</h3>
-            <p class="gallery-grid-only">${slogan}</p>
-            <img src="images/${image_file}" alt="logo of ${name}" loading="lazy" width="340" height="440" class="gallery-grid-only" ${loadingAttr} ${priorityAttr}>
-            <p>${address}</p>
-            <p>${phone_number}</p>
-            <p>${website_url}</p>
-            <p>${membership_level}</p>
-        `;
+                <h3>${name}</h3>
+                <p class="gallery-grid-only">${slogan}</p>
+                <img src="images/${image_file}" alt="logo of ${name}" loading="lazy" width="340" height="440" class="gallery-grid-only">
+                <p>${address}</p>
+                <p>${phone_number}</p>
+                <p>${website_url}</p>
+                <p>${membership_level}</p>
+            `;
 
             fragment.appendChild(card);
         });
