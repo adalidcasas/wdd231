28daf6c6c3ed43483cb24f097c9771ab2518e416
adalidcasas <cas19040@byui.edu>
2025-06-19 21:12:57 -0400

    m

--- finalproject/scripts/about.mjs
+++ finalproject/scripts/about.mjs
@@ -1,9 +1,11 @@
 import { initMenuToggle } from './menu.mjs';
 import { initNavigation } from './navigation.mjs';
 import { setFooterInfo } from './footerinfo.mjs';
+import { lastVisit } from './visitmessage.mjs';
 
 document.addEventListener("DOMContentLoaded", () => {
     initMenuToggle();
     initNavigation();
     setFooterInfo();
+    lastVisit();
 });
